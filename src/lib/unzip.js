e851cecc80f23573267541c275fcfbd9977bdb7a
Cory Brown <oh.wise.man@gmail.com>
2016-04-03 10:53:21 -0600

    lint

--- src/lib/unzip.js
+++ src/lib/unzip.js
@@ -1,6 +1,6 @@
 import extract from 'extract-zip';
 
-export default function(zipfile, dir) {
+export default function (zipfile, dir) {
     return new Promise((resolve, reject) => {
         extract(zipfile, { dir }, err => {
             return err ? reject(err) : resolve(dir);
